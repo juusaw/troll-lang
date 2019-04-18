d0e86e3b8c8852f7368436ef99b2027853c8e2cb
Juuso Mikkonen <hello@juusomikkonen.com>
2019-04-18 08:54:08 +0300

    Update build script to support both native and js compilation

--- build.sh
+++ build.sh
@@ -1,3 +1,19 @@
 #!/bin/bash 
-corebuild -use-menhir src/cli.native
-mv cli.native troll
+case "$1" in
+  ""|"native")
+    echo "native"
+    corebuild -use-menhir src/cli.native
+    mv cli.native troll
+    ;;
+  "javascript")
+    echo "javascript"
+    ocamlbuild -use-ocamlfind -pkgs 'base,js_of_ocaml,js_of_ocaml.ppx' -use-menhir src/javascript.byte
+    js_of_ocaml +base/runtime.js javascript.byte
+    mv javascript.js troll.js
+    ;;
+  *)
+    echo "Illegal argument to build script"
+    exit 1
+    ;;
+esac
+exit 0
