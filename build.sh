e519009d6b25d872b675f0a5a50dca2c3326f19c
Juuso Mikkonen <hello@juusomikkonen.com>
2019-04-22 18:31:17 +0300

    Improve logging in build script

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash 
 case "$1" in
   ""|"native")
-    echo "native"
+    echo "Compiling to native binary..."
     corebuild -use-menhir src/cli.native
     mv cli.native troll
     ;;
   "javascript")
-    echo "javascript"
+    echo "Compiling to Javascript..."
     ocamlbuild -use-ocamlfind -pkgs 'base,js_of_ocaml,js_of_ocaml.ppx' -use-menhir src/javascript.byte
     js_of_ocaml +base/runtime.js javascript.byte
     mv javascript.js troll.js
